c9f46a82acebbaa48558b5f4c9b863f3298a8bca
Astrid Konicki <astrid.konicki@mymail.champlain.edu>
2025-11-15 16:03:40 -0500

    Create structs for assignment statements

--- src/parser/expression.rs
+++ src/parser/expression.rs
@@ -1,4 +1,21 @@
 use std::fmt::Debug;
 
 #[allow(unused)]
-pub trait Expression: Debug {}
+pub enum EvaluationResult {
+    Number,
+    String,
+    Decimal,
+    Boolean,
+    // I might introduce support for these types later on
+    /*
+    Array,
+    Object,
+    Function,
+    Null,
+     */
+}
+
+#[allow(unused)]
+pub trait Expression: Debug {
+    fn get_evaluation_type(&self) -> EvaluationResult;
+}
--- src/parser/statement.rs
+++ src/parser/statement.rs
@@ -1,8 +1,27 @@
-use crate::tokenizer::tokens::Token;
+use crate::tokenizer::tokens::*;
 use std::fmt::Debug;
+use crate::parser::expression::{EvaluationResult, Expression};
 
 pub trait Statement: Debug {}
 
+#[allow(unused)]
 pub struct LetStatement {
-    pub identifier: Token,
+    pub identifier: IdentifierData,
+    // optional because we can also have type inference
+    pub evaluation_type: EvaluationResult,
+    pub expression: dyn Expression
+}
+
+#[allow(unused)]
+pub struct AssignmentStatement {
+    pub identifier: IdentifierData,
+    pub evaluation_type: EvaluationResult,
+    pub expression: dyn Expression
+}
+
+#[allow(unused)]
+pub struct ConstStatement {
+    pub identifier: IdentifierData,
+    pub evaluation_type: EvaluationResult,
+    pub expression: dyn Expression
 }
